df0bdc8697acf8f32ccd6a0c8f8076371c8a92a8
Amy <ymresume@163.com>
2018-03-20 09:29:53 +0800

    Amy

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <button class="btn" @click="showpicker">点击显示城市选择器</button>
+    <button class="amiesBtn" @click="showpicker">点击显示城市选择器</button>
     <vue-city-amies :show="showCityPicker" @confirmCity="showCity" @close="closePicker"></vue-city-amies>
-    <div class="txt">您选择的城市是： {{city}}</div>
+    <div class="amiesTxt">您选择的城市是： {{city}}</div>
   </div>
 </template>
 
@@ -31,38 +31,12 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-.btn{
+.amiesBtn{
   width: 400px;
   height: 100px;
   font-size: 40px;
 }
-.txt{
+.amiesTxt{
   font-size: 40px;
   margin-top: 30px;
 }
